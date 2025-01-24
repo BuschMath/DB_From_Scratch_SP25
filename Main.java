c44a624a86db9734dcb9504eebf3faa81d8d403a
BuschMath <busch@iowacentral.edu>
2025-01-24 10:49:18 -0600

    Test write and read int

--- Main.java
+++ Main.java
@@ -3,5 +3,12 @@ public class Main {
         Page page = new Page(1, new byte[100]);
 
         System.out.println("Page ID: " + page.getPageId());
+
+        // Example of writing and reading an integer
+        page.writeInt(0, 42);
+        page.writeInt(4, 84);
+
+        System.out.println("Should be 84: " + page.readInt(4)); // Should return 84
+        System.out.println("Should be 42: " + page.readInt(0)); // Should return 42
     }
 }
